9318271a5afd01e4ba2dfcf61daca81e46055143
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-20 15:55:10 -0600

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,30 @@
 import "./Header.scss";
+import { Link } from "react-router-dom";
+import DropDownMenu from "../DropDownMenu/DropDownMenu";
+import codeIcon from "../../assets/images/code.png";
 
 function Header() {
-  return <p>Test</p>;
+  return (
+    <div className="header__wrapper">
+      <div className="header">
+        <div className="header__container">
+          <Link to="/" className="header__logo">
+            <img
+              src={codeIcon}
+              alt=""
+              width="30"
+              height="30"
+              className="header__img"
+            />
+            <p>Tamara Carlos</p>
+          </Link>
+          <div>
+            <DropDownMenu />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
