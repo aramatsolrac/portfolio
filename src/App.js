9c39736772e856f420e39704b3530ab00681ae20
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-20 21:00:08 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.scss";
+import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects/Projects";
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Skills />
       <Projects />
+      <Contact />
     </>
   );
 }
