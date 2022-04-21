ad2137c77c34b88c490d9cb59a1f4049eca8dcb8
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-20 20:09:03 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Skills />
+      <Projects />
+    </>
+  );
 }
 
 export default App;
