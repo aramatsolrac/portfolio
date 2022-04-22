3d8c478fbdfb3bcee8401d12014f2e792602d893
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-21 20:09:16 -0600

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,27 @@
 import "./Header.scss";
 import { Link } from "react-router-dom";
-import DropDownMenu from "../DropDownMenu/DropDownMenu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
+import DropDownMenu from "../DropDownMenu/DropDownMenu";
+import Menu from "../Menu/Menu";
 
 function Header(props) {
-  // return (
-  //   <div className="header__wrapper">
-  //     <div className="header">
-  //       <div className="header__container">
-  //         <Link to="/" className="header__logo">
-  //           <FontAwesomeIcon icon={faCode} className="header__icon" />
-  //           <p>Tamara Carlos</p>
-  //         </Link>
-  //         <div className="header__menu">
-  //           <div>
-  //             <button onClick={props.toggleTheme}>
-  //               {props.theme === "light" ? (
-  //                 <FontAwesomeIcon icon={faMoon} size="lg" />
-  //               ) : (
-  //                 <FontAwesomeIcon icon={faSun} size="lg" />
-  //               )}
-  //             </button>
-  //           </div>
-  //           <DropDownMenu />
-  //         </div>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+  const [isMobile, setIsMobile] = useState(false);
+  //choose the screen size
+  const handleResize = () => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  // create an event listener
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  });
+
   return (
     <div className="header__wrapper">
       <div className="header">
@@ -38,6 +31,7 @@ function Header(props) {
             <p>Tamara Carlos</p>
           </Link>
           <div className="header__menu">
+            <div>{isMobile ? null : <Menu />}</div>
             <div>
               <button onClick={props.toggleTheme} className="header__button">
                 {props.theme === "light" ? (
@@ -47,7 +41,7 @@ function Header(props) {
                 )}
               </button>
             </div>
-            <DropDownMenu />
+            <div>{isMobile ? <DropDownMenu /> : null}</div>
           </div>
         </div>
       </div>
