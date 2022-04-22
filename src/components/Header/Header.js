893c5f52f074ed32c4b2249f425de560ba8f36be
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-21 18:45:43 -0600

    Add toggleTheme icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,52 @@
 import "./Header.scss";
 import { Link } from "react-router-dom";
 import DropDownMenu from "../DropDownMenu/DropDownMenu";
-import codeIcon from "../../assets/images/code.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
-function Header() {
+function Header(props) {
+  // return (
+  //   <div className="header__wrapper">
+  //     <div className="header">
+  //       <div className="header__container">
+  //         <Link to="/" className="header__logo">
+  //           <FontAwesomeIcon icon={faCode} className="header__icon" />
+  //           <p>Tamara Carlos</p>
+  //         </Link>
+  //         <div className="header__menu">
+  //           <div>
+  //             <button onClick={props.toggleTheme}>
+  //               {props.theme === "light" ? (
+  //                 <FontAwesomeIcon icon={faMoon} size="lg" />
+  //               ) : (
+  //                 <FontAwesomeIcon icon={faSun} size="lg" />
+  //               )}
+  //             </button>
+  //           </div>
+  //           <DropDownMenu />
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
   return (
     <div className="header__wrapper">
       <div className="header">
         <div className="header__container">
           <Link to="/" className="header__logo">
-            <img
-              src={codeIcon}
-              alt=""
-              width="30"
-              height="30"
-              className="header__img"
-            />
+            <FontAwesomeIcon icon={faCode} className="header__icon" />
             <p>Tamara Carlos</p>
           </Link>
-          <div>
+          <div className="header__menu">
+            <div>
+              <button onClick={props.toggleTheme} className="header__button">
+                {props.theme === "light" ? (
+                  <FontAwesomeIcon icon={faMoon} size="xl" />
+                ) : (
+                  <FontAwesomeIcon icon={faSun} size="xl" />
+                )}
+              </button>
+            </div>
             <DropDownMenu />
           </div>
         </div>
