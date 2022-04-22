34314e9675703f433bd1560a8dc10207a73a077b
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-21 18:44:43 -0600

    Add font awesome icons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,37 +1,40 @@
 import "./Contact.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import linkedinIcon from "../../assets/images/linkedin.png";
+import { faLinkedin } from "@fortawesome/fontawesome-free-brands";
 
 function Contact() {
   return (
-    <div className="contact">
+    <div className="contact" id="contact">
       <div className="contact__wrapper">
-        <div>
-          <h1 className="contact__title">Contact</h1>
-          <p className="contact__text">
-            {`I am currently open to new opportunities. If you have a question or
+        <h1 className="contact__title">Contact</h1>
+        <p className="contact__text">
+          {`I am currently open to new opportunities. If you have a question or
             just want to say hello, talk about code, pets, hiking, or even
             expand your network, please contact me and I will be delighted to
             talk with you! :)`}
-          </p>
-          <div className="contact__icons">
-            <FontAwesomeIcon
-              icon={faEnvelope}
-              size="xl"
-              onClick={() =>
-                window.open(`mailto:tamaraaselau@gmail.com`, `_blank`)
-              }
-            />
+        </p>
+        <div className="contact__social">
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            className="contact__icon"
+            onClick={() =>
+              window.open(`mailto:tamaraaselau@gmail.com`, `_blank`)
+            }
+            size="xl"
+          />
 
-            <a
-              href="https://www.linkedin.com/in/tamaracarlos/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={linkedinIcon} alt="linkedin" width={22} />
-            </a>
-          </div>
+          <a
+            href="https://www.linkedin.com/in/tamaracarlos/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="contact__icon"
+              size="xl"
+            />
+          </a>
         </div>
       </div>
     </div>
